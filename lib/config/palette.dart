d21d07c3a0f3cb7095858b3b836f7fd07a7550e3
Luu-Quan Thom Huynh <thom.huynh06@gmail.com>
2021-05-31 00:19:44 +0200

    color palette

---
+++ lib/config/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color background = Color(0xff121212);
+  static const Color surface = Color(0xff1535353);
+  static const Color onSurface = Color(0xffb3b3b3);
+
+  static const Color primary = Color(0xff1DB954); // spotify green
+}

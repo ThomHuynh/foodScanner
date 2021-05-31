3a91dd3cb853508e7f6ab459b7ee3eed75754c85
Luu-Quan Thom Huynh <thom.huynh06@gmail.com>
2021-05-31 22:17:01 +0200

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_scanner/config/palette.dart';
 import 'package:food_scanner/screens/startScreen.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +9,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Food Scanner',
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        // primarySwatch: ,
+        primaryColor: Palette.primary,
+        accentColor: Palette.primary,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      themeMode: ThemeMode.dark,
       home: StartScreen(),
     );
   }

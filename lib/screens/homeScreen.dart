b55563e91e6c35a88f13cd3f6aacb094bcfdcdef
Luu-Quan Thom Huynh <thom.huynh06@gmail.com>
2021-06-03 10:35:04 +0200

    design improvements home page

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -25,23 +25,22 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(18),
-        child: ListView(children: [
-          Container(
-            child: ListView(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              children: [
-                TextField(
+      body: ListView(children: [
+        Container(
+          child: ListView(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 18),
+                child: TextField(
                     cursorColor: Palette.primary,
                     controller: foodSearchController,
                     decoration: InputDecoration(
                       fillColor: Palette.bars,
                       filled: true,
-                      contentPadding: EdgeInsets.all(0),
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
                         borderSide: BorderSide.none,
                       ),
                       hintText: 'Enter food name...',
@@ -56,16 +55,17 @@ class _HomeScreenState extends State<HomeScreen> {
                         _futureSearchResult = getSearchResults(value);
                       });
                     }),
-                Container(
-                  child: FoodList(_futureSearchResult),
-                  padding: EdgeInsets.only(top: 18),
-                )
-              ],
-            ),
-            margin: EdgeInsets.only(top: 18),
+              ),
+              Container(
+                child: FoodList(_futureSearchResult),
+                margin: EdgeInsets.symmetric(horizontal: 18),
+                padding: EdgeInsets.only(top: 18),
+              )
+            ],
           ),
-        ]),
-      ),
+          margin: EdgeInsets.only(top: 18),
+        ),
+      ]),
     );
   }
 }
